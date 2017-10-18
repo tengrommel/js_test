71125f40348fe80683d496afc9b25ec7fd10f201
tengrommel <wwwsousouccom@126.com>
2017-10-18 16:42:44 +0800

    both & either

--- Ramda_in_ation/ex/2_combining_functions.js
+++ Ramda_in_ation/ex/2_combining_functions.js
@@ -20,3 +20,22 @@ console.log(R.find(R.complement(isEven), [1, 2, 3, 4]))
 const isOdd = R.complement(isEven_ramda)
 console.log(R.find(isOdd, [1, 2, 3, 4]))
     // 注意，complement 以函数的方式实现了逻辑非操作（!， not）的功能。
+
+
+// Both/Either
+// 一下代码为逻辑
+const wasBornInCountry = person => person.birthCountry == OUT_COUNTRY
+const wasNaturalized = person => Boolean(person.naturalizationDate)
+const isOver18 = person => person.age >= 18
+const isCitizen = person => wasBornInCountry(person) || wasNaturalized(person)
+const isEligibleToVote = person => isOver18(person) && isCitizen(perosn)
+    /*
+    both 接受两个函数，返回一个新函数：当两个传入函数都返回 true 值时，新函数返回 true，否则返回 false
+    either 接受两个函数，返回一个新函数：当两个传入函数任意一个返回 true 值时，新函数返回 true，否则返回 false
+    */
+const isCitizen_ramda = R.either(wasBornInCountry, wasNaturalized)
+const isEligibleToVote_ramda = R.both(isOver18, isCitizen)
+
+/*
+Ramda 还提供了 allPass 和 anyPass，接受由任意多个函数组成的数组作为参数。如名称所示，allPass 类似于 both，而 anyPass 类似于 either。
+*/
