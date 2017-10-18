7edd29fb594cc9e8d883d6a696fad73a23d6e2e6
tengrommel <wwwsousouccom@126.com>
2017-10-18 18:05:32 +0800

    ok

--- Ramda_in_ation/ex/3_partial_application.js
+++ Ramda_in_ation/ex/3_partial_application.js
@@ -81,4 +81,19 @@ const titlesForYear_partial_right = (books, year) => {
  在纯函数式语言中，柯里化函数在调用时，语法上看起来和调用多个参数没有什么区别。
  在 Ramda 中，一个柯里化的函数只能用其参数的子集来调用，它会返回一个接受其余参数的新函数。
  当使用它的所有参数调用，真正的原函数将被调用。
-*/
+ 柯里化的函数在下列两种情况下工作的都很好：
+    可以按正常情况下使用所有参数调用它，它可以像普通函数一样正常工作；
+    也可以使用部分参数来调用它，这时它会像使用 partial 一样工作。
+    
+    注意，这种灵活性带来了一些性能上的损失，因为 curry 需要搞清楚函数的调用方式，然后确定该做什么。
+ 一般来说，我只有需要在多个地方对同一个函数使用 partial 的时候，才会对函数进行柯里化。
+
+    接下来写一个柯里化版本的 publishedInYear 函数。注意，curry 会像 partial 一样工作；并且没有 partialRight 版本的 curry 函数。
+ 对这方面后续会有更多讨论，但现在我们需要将 publishedInYear 的参数翻转一下，以便让参数 year 在最前面。
+ */
+
+const publishedInYear_curry = curry((book, year) => book.year === year)
+const titlesForYear_curry = (books, year) => {
+    const selected = R.filter(publishedInYear_curry(year), books)
+    return map(book => book.title, selected)
+}
