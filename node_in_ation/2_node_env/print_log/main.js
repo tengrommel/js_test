21ea3b8bbed727f699056481b290ae26090a30f1
teng <wwwsousoucom@126.com>
2017-10-19 22:19:00 +0800

    堆栈追踪

---
+++ node_in_ation/2_node_env/print_log/main.js
@@ -0,0 +1,7 @@
+let name = 'alex'
+let user = { name: 'alex' }
+console.log('Hello')
+console.log('Hello %s', name)
+console.log('Helo:', name)
+console.log('Hello:', user)
+console.error('Error, bad user:', user)
