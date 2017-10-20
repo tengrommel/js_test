4f806aeb252df18db1426023c6d1e9285453c7c1
tengrommel <wwwsousouccom@126.com>
2017-10-20 12:36:00 +0800

    auto

---
+++ node_in_ation/4_node_event/reflect/new_listener.js
@@ -0,0 +1,17 @@
+var util = require('util')
+var events = require('events')
+
+function EventTracker() {
+    events.EventEmitter.call(this)
+}
+
+util.inherits(EventTracker, events.EventEmitter)
+var eventTracker = new EventTracker()
+
+eventTracker.on('newListener', function(name, listener) {
+    console.log('Event name added:', name)
+})
+
+eventTracker.on('a listener', function() {
+    // This will cause 'newListener' to fire
+})
