891bcd2c11d27a476ac2f66279423c2f90e0e76e
tengrommel <wwwsousouccom@126.com>
2017-10-20 14:20:36 +0800

    stream_express

---
+++ node_in_ation/5_stream/express_stream/index.js
@@ -0,0 +1,29 @@
+var stream = require('stream')
+var util = require('util')
+var express = require('express')
+var app = express()
+
+util.inherits(StatStream, stream.Readable)
+
+function StatStream(limit) {
+    stream.Readable.call(this)
+    this.limit = limit
+}
+
+StatStream.prototype._read = function(size) {
+    if (this.limit == 0) {
+        // Done
+        this.push();
+    } else {
+        this.push(util.inspect(process.memoryUsage()))
+        this.push('n')
+        this.limit--
+    }
+}；
+
+app.get('/', function(req, res) {
+    var statStream = new StatStream(10)
+    statStream.pipe(res)
+})
+
+app.listen(3000)
