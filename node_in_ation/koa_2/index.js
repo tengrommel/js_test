9a2df5511f3201587e6cd17f7d2bc290a977b2fb
teng <wwwsousoucom@126.com>
2017-10-21 18:34:02 +0800

    logger

--- node_in_ation/koa_2/index.js
+++ node_in_ation/koa_2/index.js
@@ -1,27 +1,14 @@
 const Koa = require('koa')
 const app = new Koa()
 const port = process.env.port || 3000
+const logger = require('koa-logger')
+const bodyparser = require('koa-bodyparser')
+
+app.use(logger())
+app.use(bodyparser())
 
 app.use(async(ctx, next) => {
-    ctx.hello = 0
-    console.log(1)
-    await next()
-    console.log(11)
-})
-
-app.use(async(ctx, next) => {
-    console.log(2)
-    ctx.hello += 10
-    if (ctx.hello > 5){
-        ctx.throw('aaa', 401)       
-    }else{
-       await next()
-    }
-    console.log(22)
-})
-
-app.use((ctx) => {
-    ctx.body = ctx.hello
+    ctx.body = 'hello world'
 })
 
 
