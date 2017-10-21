5015e963bbe68824476fd540ab375d693be73687
teng <wwwsousoucom@126.com>
2017-10-22 02:43:15 +0800

    validations

--- node_in_ation/node_rest/src/modules/users/user.routes.js
+++ node_in_ation/node_rest/src/modules/users/user.routes.js
@@ -1,8 +1,9 @@
 import { Router } from 'express'
-
+import validate from 'express-validation'
 import * as userController from './user.controllers'
+import userValidation from './user.validations'
 
 const routes = new Router()
-routes.post('/signup', userController.signUp)
+routes.post('/signup', validate(userValidation), userController.signUp)
 
 export default routes
