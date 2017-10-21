a41bab550ad3f42188da5c86f659e2c64f2651a5
teng <wwwsousoucom@126.com>
2017-10-21 18:51:59 +0800

    get&post

--- node_in_ation/koa_2/routes/index.js
+++ node_in_ation/koa_2/routes/index.js
@@ -3,6 +3,10 @@ const router = new Router()
 
 router.get('/hello', async(ctx)=>{
     ctx.body = 'world'
-});
+})
+
+router.post('/hello2', async(ctx)=>{
+    ctx.body = "I am hello2"
+})
 
 module.exports = router
