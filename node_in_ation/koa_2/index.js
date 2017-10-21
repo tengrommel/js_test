98c8a96dc909ba8a15353008416b30fde09c1298
teng <wwwsousoucom@126.com>
2017-10-21 18:25:05 +0800

    第二种报错

--- node_in_ation/koa_2/index.js
+++ node_in_ation/koa_2/index.js
@@ -13,8 +13,9 @@ app.use(async(ctx, next) => {
     console.log(2)
     ctx.hello += 10
     if (ctx.hello > 5){
-        ctx.throw('你找谁', 400)
-        return 
+        let error = new Error()
+        error.status = 401
+        throw error
     }else{
        await next()
     }
