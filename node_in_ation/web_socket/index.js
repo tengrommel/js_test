3fde1bf54ed36ed7d0822a7f2a9d66973df6b8da
teng <wwwsousoucom@126.com>
2017-10-23 23:16:03 +0800

    end

--- node_in_ation/web_socket/index.js
+++ node_in_ation/web_socket/index.js
@@ -2,8 +2,8 @@ const express = require('express')
 const socket = require('socket.io')
 
 const app = express()
-const server = app.listen(4000, ()=>{
-  console.log('listening to request on ports 4000')
+const server = app.listen(4000, () => {
+    console.log('listening to request on ports 4000')
 })
 
 // Static files
@@ -12,6 +12,14 @@ app.use(express.static('public'))
 // Socket setup
 const io = socket(server)
 
-io.on('connection', (socket)=>{
-  console.log('made socket connection', socket.id)
+io.on('connection', (socket) => {
+    console.log('made socket connection', socket.id)
+
+    socket.on('chat', (data) => {
+        io.sockets.emit('chat', data)
+    })
+
+    socket.on('typing', (data) => {
+        socket.broadcast.emit('typing', data)
+    })
 })
--- node_in_ation/web_socket/public/chat.js
+++ node_in_ation/web_socket/public/chat.js
@@ -1,2 +1,33 @@
 // Make connection
-var socket = io.connect('http://localhost:4000')
+var socket = io.connect('http://localhost:4000');
+
+// Query DOM
+var message = document.getElementById('message');
+handle = document.getElementById('handle'),
+    btn = document.getElementById('send'),
+    output = document.getElementById('output'),
+    feedback = document.getElementById('feedback');
+
+// emit events
+
+btn.addEventListener('click', () => {
+    socket.emit('chat', {
+        message: message.value,
+        handle: handle.value
+    });
+});
+
+message.addEventListener('keypress', () => {
+    socket.emit('typing', handle.value);
+})
+
+// Listen for events
+socket.on('chat', (data) => {
+    feedback.innerHTML = ''
+    output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>';
+})
+
+socket.on('typing', (data) => {
+    console.log(data)
+    feedback.innerHTML = '<p><em>' + data + ' is typing a message...</em></p>'
+})
