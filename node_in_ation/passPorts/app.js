5a876763a75651490647606e8c9e5f71c5b77216
teng <wwwsousoucom@126.com>
2017-10-25 22:40:07 +0800

    passports

---
+++ node_in_ation/passPorts/app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+
+// set up view engine
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.listen(3000, () => {
+    console.log('app now listening for requests on port 3000')
+})
