c43142509b2b75ef0e66134eb64f2e6300f80719
tengrommel <wwwsousouccom@126.com>
2017-10-26 18:09:27 +0800

    注释

--- node_in_ation/koa_in/koa_data/index.js
+++ node_in_ation/koa_in/koa_data/index.js
@@ -3,10 +3,12 @@ const app = new Koa()
 
 app.use(async(ctx) => {
     let url = ctx.url
+        // 从上下文的request对象中获取
     let request = ctx.request
     let req_query = request.query
     let req_querystring = request.querystring
 
+    // 从上下文中直接获取
     let ctx_query = ctx.query
     let ctx_querystring = ctx.querystring
 
