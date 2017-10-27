4625f9245dc960176fce3729e1aecaa0fd202031
teng <wwwsousoucom@126.com>
2017-10-28 06:44:48 +0800

    create server

---
+++ node_js/stuff/app.js
@@ -0,0 +1,24 @@
+console.log('het ninjas')
+
+setTimeout(function() {
+    console.log('3 seconds have passed')
+}, 3000)
+
+let time = 0
+
+let timer = setInterval(function() {
+    time += 2
+    console.log(time + ' seconds have passed')
+    if (time > 9) {
+        clearInterval(timer)
+    }
+}, 4000)
+
+console.log(__dirname)
+console.log(__filename)
+
+// require exports
+counter = require('./count')
+let adder = function (a, b) {
+  return `The sum of the 2 number is ${a+b}`
+}
