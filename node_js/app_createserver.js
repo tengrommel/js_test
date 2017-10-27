a17291b74ce2acc6f8b994f6a4038ee20e8f9dcf
teng <wwwsousoucom@126.com>
2017-10-28 07:12:51 +0800

    stream

---
+++ node_js/app_createserver.js
@@ -0,0 +1,10 @@
+const http = require('http')
+
+const server = http.createServer(function (req, res) {
+  console.log('request was mad: ' + req.url)
+  res.writeHead(200, {'Content-Type': 'text/plain'})
+  res.end('Hey ninja')
+})
+
+server.listen(3000, '127.0.0.1')
+console.log('yo dawgs , now listening to port 3000')
