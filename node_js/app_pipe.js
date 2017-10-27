2c88cdbf32585134b04d33309138640b7e578c8f
teng <wwwsousoucom@126.com>
2017-10-28 07:25:54 +0800

    http_pipe

--- node_js/app_pipe.js
+++ node_js/app_pipe.js
@@ -1,7 +1,12 @@
 const http = require('http')
 const fs = require('fs')
 
-const myReadStream = fs.createReadStream(__dirname + '/note', 'utf8')
-const myWriteStream = fs.createWriteStream(__dirname + '/pipenote')
+const server = http.createServer(function (req, res) {
+  console.log('request was made: ' + req.url)
+  res.writeHead(200, {'Content-Type': 'text/plain'})
+  var myReadStream = fs.createReadStream(__dirname + '/pipenote', 'utf8')
+  myReadStream.pipe(res)
+})
 
-myReadStream.pipe(myWriteStream)
+server.listen(3000, '127.0.0.1')
+console.log('yo dawgs , now listening to port 3000')
