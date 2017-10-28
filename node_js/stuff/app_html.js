f082fbb6d10d3b781073a78ecbf6e143e090052e
teng <wwwsousoucom@126.com>
2017-10-28 08:15:11 +0800

    express

---
+++ node_js/stuff/app_html.js
@@ -0,0 +1,16 @@
+const http = require('http')
+const fs = require('fs')
+
+const server = http.createServer(function (req, res) {
+  console.log('request was made: ' + req.url)
+  res.writeHead(200, {'Content-Type': 'application/json'})
+  let myObj = {
+    name: 'Ryu',
+    job: 'Ninja',
+    age: 29
+  }
+  res.end(JSON.stringify(myObj))
+})
+
+server.listen(3000, '127.0.0.1')
+console.log('yo dawgs , now listening to port 3000')
