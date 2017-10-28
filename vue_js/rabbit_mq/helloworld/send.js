91b7120cd79615920fb8f4f0f39db93109dfa69e
teng <wwwsousoucom@126.com>
2017-10-28 17:56:38 +0800

    Ok rabbitmq

--- vue_js/rabbit_mq/helloworld/send.js
+++ vue_js/rabbit_mq/helloworld/send.js
@@ -6,8 +6,10 @@ amqp.connect("amqp://localhost", function(err, conn) {
   conn.createChannel(function (err, ch) {
     const q = "hello"
     ch.prefetch(2)
+
     ch.assertQueue(q, {durable: false})
-    ch.sendToQueue(q, new Buffer('{"hello": "world"}'))
+    
+    sch.sendToQueue(q, new Buffer('{"hello": "world"}'))
    // ch.publish((exchange=""), (routing_key="hello"), (body="message"))
 
     setTimeout(function () {
