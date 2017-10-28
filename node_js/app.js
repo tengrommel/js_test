71e070781fa700210ef1b58b91e2dc111f6cd5d4
teng <wwwsousoucom@126.com>
2017-10-28 10:44:21 +0800

    todo

--- node_js/app.js
+++ node_js/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('this is the homepage')
+  res.sendFile(__dirname + '/index.html')
 })
 
 app.get('/contact', function (req, res) {
