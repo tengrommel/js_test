9bbb0808d5b0275b0e9b1e2f5475bd172b717d8c
teng <wwwsousoucom@126.com>
2017-10-28 08:41:59 +0800

    express

--- node_js/app.js
+++ node_js/app.js
@@ -1,3 +1,17 @@
 const express = require('express')
 const app = express()
 
+app.get('/', function (req, res) {
+  res.send('this is the homepage')
+})
+
+app.get('/contact', function (req, res) {
+  res.send('this is the connect page')
+})
+
+app.get('/profile/:id', function (req, res) {
+  res.send('You requested to see a profile with the id of ' + req.params.id)
+})
+
+
+app.listen(3000)
